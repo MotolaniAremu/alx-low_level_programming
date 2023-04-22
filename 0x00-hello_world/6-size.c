69a7b10072dd0c365acd531c2a17edc2d9288188
MotolaniAremu <motolaniaremu@gmail.com>
2023-04-22 05:33:44 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
-#include <stdio>
+#include <stdio.h>
 /**
- * main - program that prints the size of vrious computer types
- * Return 0
+ * main - program that prints the size of various computer types
+ * Return: 0 (success)
  */
 int main(void)
 {
