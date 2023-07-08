5e19475d87d035383afc9e8d495259043c924d26
Motolani Aremu <motolaniaremu@gmail.com>
2023-07-08 06:22:41 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * @s: String
+ * Return: No return
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
